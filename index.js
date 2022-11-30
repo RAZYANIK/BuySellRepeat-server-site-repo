8bb06a730460ce86ecac870124fe0362584ba032
RAZYANIK <mdfarhanrazy@gmail.com>
2022-11-30 22:19:23 +0600

    17th  commit

--- index.js
+++ index.js
@@ -86,18 +86,20 @@ async function run() {
             const result = await addedProductCollection.deleteOne(query);
             res.send(result);
         })
-        app.get('/bookings', verifyJWT, async (req, res) => {
-            const email = req.query.email;
-            const decodedEmail = req.decoded.email;
 
-            if (email !== decodedEmail) {
-                return res.status(403).send({ message: 'forbidden access' });
-            }
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            // const decodedEmail = req.decoded.email;
+
+            // if (email !== decodedEmail) {
+            //     return res.status(403).send({ message: 'forbidden access' });
+            // }
 
             const query = { email: email };
             const bookings = await bookingsCollection.find(query).toArray();
             res.send(bookings);
         })
+
         app.get('/jwt', async (req, res) => {
             const email = req.query.email;
             const query = { email: email };
